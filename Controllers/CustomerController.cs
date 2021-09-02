11b8716e3030c1b8cce60940a6fb0c6526b93242
anith <anith@ANITHAANA3>
2021-09-02 15:32:00 -0500

    Add project files.

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Customer_Management_System.Models;
+
+namespace Customer_Management_System.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private CustomerDBEntities1 db = new CustomerDBEntities1();
+
+        public ActionResult Index()
+        {
+            return View(db.CustomerInfo.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CustomerInfo customerInfo = db.CustomerInfo.Find(id);
+            if (customerInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customerInfo);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Cust_Id,CustName,Password,Mobile_No,Gender,Age,Date_Of_Birth,Email_Id")] CustomerInfo customerInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CustomerInfo.Add(customerInfo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(customerInfo);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CustomerInfo customerInfo = db.CustomerInfo.Find(id);
+            if (customerInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customerInfo);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Cust_Id,CustName,Password,Mobile_No,Gender,Age,Date_Of_Birth,Email_Id")] CustomerInfo customerInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customerInfo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customerInfo);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CustomerInfo customerInfo = db.CustomerInfo.Find(id);
+            if (customerInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customerInfo);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CustomerInfo customerInfo = db.CustomerInfo.Find(id);
+            db.CustomerInfo.Remove(customerInfo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
